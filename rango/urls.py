4a191ae6d4813c44e18226a6f46ea859114af18c
Ziyang Zhu <2599143z@student.gla.ac.uk>
2021-07-29 15:33:33 +0100

    9.6 done

--- rango/urls.py
+++ rango/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
                     #路径里的这个变量和views里函数的参数有关
     path('add_category/', views.add_category, name='add_category'),
     path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
-
+    path('register/', views.register, name='register'),
+    
 ]
