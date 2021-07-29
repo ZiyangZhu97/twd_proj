a2aed38c639f3fbe70acb90fe58fedda2ddee09e
Ziyang Zhu <2599143z@student.gla.ac.uk>
2021-07-29 15:33:21 +0100

    9.6 done

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from rango.models import Page, Category
+from rango.models import Page, Category, UserProfile
+from django.contrib.auth.models import User
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
@@ -8,7 +9,7 @@ class CategoryForm(forms.ModelForm):
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
     class Meta:
-        model = Category
+        model = Category #models里的类名
         fields = ('name',)#显示name，不显示其他所有
 
 class PageForm(forms.ModelForm):
@@ -29,3 +30,15 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
         
         return cleaned_data
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     NAME_MAX_LENGTH = 128
@@ -22,7 +23,7 @@ class Category(models.Model):
 class Page(models.Model):
     TITLE_MAX_LENGTH = 128
     URL_MAX_LENGTH = 200
-    
+
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=TITLE_MAX_LENGTH)
     url = models.URLField()
@@ -31,4 +32,10 @@ class Page(models.Model):
     def __str__(self) -> str:
         return self.title
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
 
+    def __str__(self):
+        return self.user.username
